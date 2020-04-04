ae361c4f5570f12ecd129706dd5de556988517a8
R3ndydinar <Genjitapaleuk@gmail.com>
2020-04-04 17:58:35 +0700

    Update Data Sumba Timur 4 April 2020

--- src/components/DataSumbaTimur.js
+++ src/components/DataSumbaTimur.js
@@ -7,12 +7,12 @@ import Container from '@material-ui/core/Container';
 const infoTotal = [
 	{
 		ket: 'PELAKU PERJALANAN' , emoji: '🛬', data: {
-			total: 913
+			total: 1.064
 		}
 	},
 	{
 		ket: 'ORANG DALAM PANTAUAN (ODP)', emoji: '🕵', data: {
-			total: 36
+			total: 41
 		} 
 	},
 	{

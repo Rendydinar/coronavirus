4c56d34bce756509ae1460629bcea3ebf195c184
R3ndydinar <Genjitapaleuk@gmail.com>
2020-07-19 20:11:47 +0700

    Update data sumba timur Minggu, 19 Juli 2020

--- src/components/DataSumbaTimur.js
+++ src/components/DataSumbaTimur.js
@@ -6,7 +6,7 @@ import Container from '@material-ui/core/Container';
 const infoTotal = [
 	{
 		ket: 'TOTAL PELAKU PERJALANAN' , emoji: '🛬', data: {
-			total: '7.326'
+			total: '7.404'
 		}
 	},
 	{

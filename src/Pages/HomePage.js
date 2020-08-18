c74e0ec376eff71d395597f8eb0a8a1981032579
Rendy Dinar <r3ndydinar@gmail.com>
2020-08-18 21:27:10 +0700

    Update data sumba timur Kamis, 13 Agustus 2020

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -26,7 +26,7 @@ export default function HomePage() {
       <KasusTotal />
       <Sumber ket={{sumber: 'Indonesia COVID 19 API', waktu: `${date} WITA`, link: 'https://indonesia-covid-19.mathdro.id/api'}}/>
       <DataSumbaTimur />
-      <Sumber ket={{sumber: 'Posko Covid Sumba Timur', waktu: 'Rabu, 12 Agustus 2020 16:00 PM WITA', link: 'https://www.facebook.com/dinkes.timur'}}/>
+      <Sumber ket={{sumber: 'Posko Covid Sumba Timur', waktu: 'Kamis, 13 Agustus 2020 16:00 PM WITA', link: 'https://www.facebook.com/dinkes.timur'}}/>
       <TentangDataSumbaTimur />
       <Provinsi />
       <Sumber ket={{sumber: 'Indonesia COVID 19 API', waktu: `${date} WITA`, link: 'https://indonesia-covid-19.mathdro.id/api'}}/>

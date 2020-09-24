5b85e400f7d3587b581d88302fd7ef2b0a3941ac
Rendy Dinar <r3ndydinar@gmail.com>
2020-09-24 14:21:37 +0700

    Update Data Sumba Timur Rabu 2 September 2020

--- src/components/DataSumbaTimur.js
+++ src/components/DataSumbaTimur.js
@@ -6,7 +6,7 @@ import Container from '@material-ui/core/Container';
 const infoTotal = [
 	{
 		ket: 'TOTAL PELAKU PERJALANAN' , emoji: '🛬', data: {
-			total: '12.686'
+			total: '12.804'
 		}
 	},
 	{
